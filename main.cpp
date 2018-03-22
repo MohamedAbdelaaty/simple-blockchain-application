df75cc7b9acc26e37de858533530cdef430d62f7
Mohamed Abdelaaty <mohamed.abdelaaty@alumni.ubc.ca>
2018-03-22 14:51:54 -0700

    updated main file to show an example

--- main.cpp
+++ main.cpp
@@ -3,5 +3,22 @@
 int main() {
 	Blockchain ourBlockChain();
 
+	/*
+	*
+	*	adding 5 blocks without proof of work
+	*
+	*/
+
+
+	for (int i = 0; i < 5; i++) {
+		std::string data = "This is block number " + (i + 1);
+		Block blockToAdd(ourBlockChain().getNextChainIndex(), data, ourBlockChain().fetchLastBlock().getBlockHash());
+
+	}
+	
+	for (int i = 0; i < ourBlockChain().getNextChainIndex(); i++)
+	{
+		std::cout << ourBlockChain().fetchBlock(i).getBlockHash() << std::endl;
+	}
 	return 0;
 }
