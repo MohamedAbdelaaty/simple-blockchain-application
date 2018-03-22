540ce16324f2db432551476949cfc4acb8757a4c
Mohamed Abdelaaty <mohamed.abdelaaty@alumni.ubc.ca>
2018-03-22 00:42:19 -0700

    Comments updated with minor updates
    Updated comments and replaces the chain.addBlock(Block) from teh generating the genesis block and used the addBlock function for consistency

--- Blockchain.cpp
+++ Blockchain.cpp
@@ -1,18 +1,21 @@
 #include "Blockchain.h"
 
 Blockchain::Blockchain() {
-	generateGenesisBlock();
+	generateGenesisBlock();			// the beginning of any blockchain requires a genesis block which is generated in the constructor as a start poing for the chain
 }
 
 void Blockchain::addBlock(Block blockToAdd)
 {
-	chain.push_back(blockToAdd);
+	chain.push_back(blockToAdd);		// function to add block to the chain, missing a proof of work algorithm which will be implemented over time
 
 	return;
 }
 
 void Blockchain::generateGenesisBlock() {
-	Block genesisBlock(0, "genesis data", "000");
-	chain.push_back(genesisBlock);
+	Block genesisBlock(0, "genesis data", "000");	// generating the genesis block using initialization data
+							// index = 0
+							// Data = "genesis data"
+							// Previous block hash = "000"
+	addBlock(genesisBlock);				// adds block to chain
 }
 
