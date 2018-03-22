c3543736d0f8e3d8ddc1f7d165d1874407256cc5
Mohamed Abdelaaty <mohamed.abdelaaty@alumni.ubc.ca>
2018-03-22 00:38:56 -0700

    Updates comments for Block.h
    Updated the comments for Block.h to help understand the technology

--- Block.h
+++ Block.h
@@ -1,19 +1,21 @@
 #pragma once
 #include "Dependencies.h"
 
+// class definition for the data Block
 class Block {
 public:
-	Block(int index, std::string data, std::string previousBlockHash);
-	std::string getBlockHash();
-	std::string getPreviousHash();
-	std::string getData();
-	std::time_t getUnixTimestamp();
+	Block(int index, std::string data, std::string previousBlockHash);	// Class constructor
+	std::string getBlockHash();						// returns the block hash stored in the private variable
+										// blockHash
+	std::string getPreviousHash();						// returns the previous block hash stored in the private variable
+	std::string getData();							// returns the data stored in the private variable\
+	std::time_t getUnixTimestamp();						// returns the time stamp of block creation
 private:
-	int index;
-	std::string data;
-	std::string blockHash;
-	std::string previousBlockHash;
-	std::time_t unixTimestamp = std::time(nullptr);
+	int index;								// block index number
+	std::string data;							// data stored inside a block
+	std::string blockHash;							// block hash generated using sha256 encryption
+	std::string previousBlockHash;						// previous block hash to maintain integrity of the chain
+	std::time_t unixTimestamp = std::time(nullptr);				// unix time stamp representing the time the block was generated
 	
-	std::string generateBlockHash(std::string, std::string);
+	std::string generateBlockHash(std::string, std::string);		// Function to generate the block hash
 };
