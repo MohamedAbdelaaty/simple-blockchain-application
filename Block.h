4a278099010afa172dc0472ad5bf6bd7cc8ff41d
Mohamed Abdelaaty <mohamed.abdelaaty@alumni.ubc.ca>
2018-04-04 17:13:37 -0700

    Fixed header loophole and added header comment

--- Block.h
+++ Block.h
@@ -1,5 +1,22 @@
+/*
+	Created by: Mohamed Abdelaaty
+	
+	Created On: March 17th
+	
+	Published On: March 21st
+	
+	Last updated: April 4th
+	
+	DISCLAIMER: this was made for educational, non-profit purposes only.
+*/
+
 #pragma once
-#include "Dependencies.h"
+#include <string>
+#include <vector>
+#include <ctime>
+#include "sha256.h"
+#include "Blockchain.h"
+#include <iostream>
 
 // class definition for the data Block
 class Block {
