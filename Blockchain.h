ed6ee8b227a2c53e35ce3fcc9aded5ee41ce661c
Mohamed Abdelaaty <mohamed.abdelaaty@alumni.ubc.ca>
2018-04-04 17:11:25 -0700

    Fixed dependencies loophole

--- Blockchain.h
+++ Blockchain.h
@@ -1,5 +1,24 @@
+/*
+
+	Created by: Mohamed Abdelaaty
+	
+	Created On: March 17th
+	
+	Published On: March 21st
+	
+	Last updated: April 4th
+	
+	DISCLAIMER: this was made for educational, non-profit purposes only.
+
+*/
+
 #pragma once
-#include "Dependencies.h"
+#include <string>
+#include <vector>
+#include "Block.h"
+#include <ctime>
+#include "sha256.h"
+#include <iostream>
 
 class Blockchain {
 public:
